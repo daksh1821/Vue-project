a6ab57951974b683336e0199fb831226119c4ed3
daksh1821 <e22cseu1238@bennett.edu.in>
2025-10-27 21:35:08 +0530

    reactive function

--- vue-project/src/components/myevent.vue
+++ vue-project/src/components/myevent.vue
@@ -1,14 +1,17 @@
 <script setup>
-import { ref } from 'vue';
-let count = ref(0);
-const inc = () =>{
-  count.value++;
+import { reactive } from 'vue';
+let initial_state = reactive({val: {count:0}, user:["daksh", "jain"]});
+const changeuser = () =>{
+  initial_state.user[0] = "khushi";
+  initial_state.user[1] = "pathak";
 }
 </script>
 
 <template>
-<h1>Count : {{count}}</h1> 
-<button @:click="inc">Increment</button>
+<h1>Count : {{initial_state.val.count}}</h1>
+<h1>users: {{initial_state.user}}</h1>
+<button @:click="initial_state.val.count++">Increment</button>
+<button @:click="changeuser"> change user</button>
 </template>
 
 <style scoped></style>
