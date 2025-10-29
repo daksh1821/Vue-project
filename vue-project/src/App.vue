cc930feaf8311e220e25361e63090374793f9746
daksh1821 <e22cseu1238@bennett.edu.in>
2025-10-29 16:04:14 +0530

    lifecycle hooks

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,13 +1,7 @@
 <script setup>
-import { ref } from 'vue';
-const first_name = ref("daksh");
-const last_name = ref("jain");
-import staticpropscomponents from './components/staticpropscomponents.vue';
-import dynamicprops from './components/dynamicprops.vue';
+import lifecyclehooks from './components/lifecyclehooks.vue'
 </script>
 
 <template>
-  <staticpropscomponents name="daksh"/>
-  <hr>
-  <dynamicprops :first_name="first_name" :last_name="last_name"/>
+  <lifecyclehooks/>
 </template>
---
+++ vue-project/src/components/lifecyclehooks.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { ref ,onBeforeMount ,onBeforeUpdate,onMounted, onUpdated, onBeforeUnmount, onUnmounted} from 'vue';
+const messege = ref("hello");
+</script>
+
+<template>
+<p>{{messege}}</p>
+</template>
