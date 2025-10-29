6ad1b5e2c14baf7b3fce0d271bbddf0c2fcd3021
daksh1821 <e22cseu1238@bennett.edu.in>
2025-10-29 09:59:42 +0530

    dynamic props

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
+import { ref } from 'vue';
+const first_name = ref("daksh");
+const last_name = ref("jain");
 import staticpropscomponents from './components/staticpropscomponents.vue';
+import dynamicprops from './components/dynamicprops.vue';
 </script>
 
 <template>
   <staticpropscomponents name="daksh"/>
+  <hr>
+  <dynamicprops :first_name="first_name" :last_name="last_name"/>
 </template>
---
+++ vue-project/src/components/dynamicprops.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+const props = defineProps(['first_name', 'last_name'])
+console.log(props)
+</script>
+
+<template>
+  <h1>{{ props.first_name }}</h1>
+  <h1>{{ props.last_name }}</h1>
+</template>
+<style scoped></style>
